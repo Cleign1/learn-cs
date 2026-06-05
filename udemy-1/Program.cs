403ff589413efd54809563274a6275664ce51a10
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-05 15:50:39 +0700

    testing course 27

--- udemy-1/Program.cs
+++ udemy-1/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string? userInput1 = Console.ReadLine();
-Console.WriteLine("Input the first Number " + userInput1);
+string[] names;
 
-string? userInput2 = Console.ReadLine();
-Console.WriteLine($"Input Second Number {userInput2}");
+names = ["Ibnu", "Kevin", "Cleign", "Yata", "Yudha", "Reza"];
 
+Random.Shared.Shuffle(names);
+
+for (int i = 0; i < names.Length; i++)
+{
+  Console.WriteLine(names[i]);
+}
