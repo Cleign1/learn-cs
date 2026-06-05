b42ad2b33903c067c3ad903924887e5ef7de8f4f
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-05 10:53:46 +0700

    course 16 udemy

--- udemy-1/Program.cs
+++ udemy-1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string name = "Ibnu Khaidar";
+
+Console.WriteLine($"Hello! {name} \n\n");
+
+name = "Cleign";
+Console.WriteLine($"Whops, your name changed to {name}");
