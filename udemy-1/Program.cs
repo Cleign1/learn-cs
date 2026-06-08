993d25e1426b6c81be54f240405e505eded04414
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-08 13:02:24 +0700

    course 66

--- udemy-1/Program.cs
+++ udemy-1/Program.cs
@@ -1,6 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int num = 10;
-double price = 99.99;
-string name = "Frank";
+int day = 3;
 
-Console.WriteLine($"The number is {num}, the price is {price}, and the name is {name}.");
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Invalid day");
+        break;
+}
