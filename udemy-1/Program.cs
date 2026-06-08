fe88756326100fc40481ff027f5f8b7fdadf66c8
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-08 11:14:33 +0700

    course 39 done

--- udemy-1/Program.cs
+++ udemy-1/Program.cs
@@ -1,20 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// implicit conversion
-int myInt;
-myInt = 2147483647;
-double myDouble;
-myDouble = myInt;
+// conversion
+string numberString = "1234";
+int number = int.Parse(numberString);
+Console.WriteLine(number);
 
-Console.WriteLine(myInt);
-
-
-// explicit conversion
-long myLong = 3242343234324234323;
-Console.WriteLine(myLong);
-int myInt2 = (int)myLong;
-Console.WriteLine(myInt2);
-
-float myFloat = 3.14f;
-myDouble = 112.1232132132312f;
-myFloat = (float)myDouble;
-Console.WriteLine(myFloat);
+string boolString = "true";
+bool boolValue = Convert.ToBoolean(boolString);
+Console.WriteLine(boolValue);
