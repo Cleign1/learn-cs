95e31087a44e3eac662ac1ebb5806388051fae00
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-09 10:48:44 +0700

    course 75

--- udemy-1/Program.cs
+++ udemy-1/Program.cs
@@ -1,40 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // conditional based calculator
 
-Console.WriteLine("Enter the first number: ");
-double num1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Enter the second number: ");
-double num2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Choose an operation: +, -, *, /");
-string op = Console.ReadLine();
-double result = 0;
-if (op == "+")
+for (int counter = 10; counter >= 0; counter--)
 {
-    result = num1 + num2;
+    Console.WriteLine($"Count : {counter}");
 }
-else if (op == "-")
-{
-    result = num1 - num2;
-}
-else if (op == "*")
-{
-    result = num1 * num2;
-}
-else if (op == "/")
-{
-    if (num2 != 0)
-    {
-        result = num1 / num2;
-    }
-    else
-    {
-        Console.WriteLine("Cannot divide by zero.");
-        return;
-    }
-}
-else
-{
-    Console.WriteLine("Invalid operator.");
-    return;
-}
-Console.WriteLine($"Result: {result}");
