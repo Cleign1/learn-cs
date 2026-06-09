58afb9cc21f64eec28b60e6848d873a02b5b1c05
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-09 13:20:22 +0700

    course 92

--- udemy-1/Program.cs
+++ udemy-1/Program.cs
@@ -1,7 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // conditional based calculator
-
-for (int counter = 10; counter >= 0; counter--)
+int[,] twoDimensional = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+            };
+// sum of every row
+for (int i = 0; i < twoDimensional.GetLength(0); i++)
 {
-    Console.WriteLine($"Count : {counter}");
+    int sum = 0;
+    for (int j = 0; j < twoDimensional.GetLength(1); j++)
+    {
+        sum += twoDimensional[i, j];
+    }
+    Console.WriteLine($"Sum of row {i} is: {sum}");
 }
