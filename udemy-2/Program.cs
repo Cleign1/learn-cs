6959d500312c81efea0b153eef9486bebbef84e6
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-10 12:05:38 +0700

    course 107

--- udemy-2/Program.cs
+++ udemy-2/Program.cs
@@ -20,6 +20,9 @@ class Program
 
         double averageTemperature = CalculateAverageTemperature(temperatures);
         Console.WriteLine($"Average Temperature: {averageTemperature:F2}°C");
+        Console.WriteLine($"The Max Temperature: {temperatures.Max()}°C");
+        Console.WriteLine($"The Min Temperature: {temperatures.Min()}°C");
+        Console.WriteLine($"The Most Common Temprature: {MostCommonWeatherCondition(weatherConditions)}");
     }
 
     static double CalculateAverageTemperature(int[] temperatures)
@@ -30,5 +33,29 @@ class Program
             sum += temp;
         }
         return (double)sum / temperatures.Length;
+    }
+
+    static string MostCommonWeatherCondition(string[] weatherConditions)
+    {
+        int count = 0;
+        string mostCommon = weatherConditions[0];
+
+        for (int i = 0; i < weatherConditions.Length; i++)
+        {
+            int tempCount = 0;
+            for (int j = 0; j < weatherConditions.Length; j++)
+            {
+                if (weatherConditions[j] == weatherConditions[i])
+                {
+                    tempCount++;
+                }
+                if (tempCount > count)
+                {
+                    count = tempCount;
+                    mostCommon = weatherConditions[i];
+                }
+            }
+        }
+        return mostCommon;
     }
 }
