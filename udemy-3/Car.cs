ee73f9b84cef9e77860764378aa87ccfd338ebbe
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-10 16:08:08 +0700

    course 115

--- udemy-3/Car.cs
+++ udemy-3/Car.cs
@@ -4,10 +4,30 @@ public class Car
 {
     private string _model = "";
     private string _brand = "";
+
+    public string Model { get => _model; set =>  _model = value; }
+
+    public string Brand
+    {
+        get => _brand;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {   
+                Console.WriteLine("A car brand is required.");
+                throw new ArgumentException("Brand name cannot be empty");
+            }
+            else
+            {
+                _brand = value;
+            }
+        }
+    }
+
     public Car(string model, string brand)
     {
-        _model = model;
-        _brand = brand;
-        Console.WriteLine($"A car brand {_brand} with {_model} model has been created.");
+        Model = model;
+        Brand = brand;
+        Console.WriteLine($"A car brand {_brand} with {Model} model has been created.");
     }
 }
--- udemy-3/Program.cs
+++ udemy-3/Program.cs
@@ -6,6 +6,9 @@ class Program
     {
         Car audi = new Car("Audi", "A3");
         Car bmw = new Car("BMW", "M5");
+        
+        Console.WriteLine("Please Enter the Brand name");
+        audi.Brand = Console.ReadLine() ?? "Audi";
         Console.WriteLine(audi);
         Console.WriteLine(bmw);
     }
