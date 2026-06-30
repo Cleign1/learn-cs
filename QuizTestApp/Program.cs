21e1573739aa016bf43f9fd8b228dace2e548e40
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-30 12:33:52 +0700

    course 141new quizz app

---
+++ QuizTestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ QuizTestApp/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizTestApp
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] Answers { get; set; }
+        public int CorrectionAnswer { get; set; }
+
+        public Question(string questionText, string[] answers, int correctAnswerIndex)
+        {
+            QuestionText = questionText;
+            Answers = answers;
+            CorrectionAnswer = correctAnswerIndex;
+        }
+
+        public bool IsCorrectAnswer(int choice)
+        {
+            return CorrectionAnswer == choice;
+        }
+    }
+}
