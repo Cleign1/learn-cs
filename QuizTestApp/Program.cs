080acdedb8ed298e8319de110233b26856bbdbf5
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-06-30 22:26:32 +0700

    course 143

--- QuizTestApp/Program.cs
+++ QuizTestApp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Question[] question = new Question[]
+            {
+                new Question("Siapa Gweh?",
+                new string[] {"Gweh", "Lu", "Awas ada suleee"},
+                1)
+            };
+
+            Quiz myQuiz = new Quiz(question);
+            myQuiz.DisplayQuestion(question[0]);
+
+
         }
     }
 }
---
+++ QuizTestApp/Quiz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizTestApp
+{
+    internal class Quiz
+    {
+        private Question[] question;
+        public Quiz(Question[] questions)
+        {
+            this.question = questions;
+        }
+
+        public void DisplayQuestion(Question question)
+        {
+            Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.Write($"{i+1}");
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+        }
+    }
+}
