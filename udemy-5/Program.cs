1e3811edc16e9534faf783b3dcf1c6abcced9d4e
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-07-01 13:21:27 +0700

    course 164

---
+++ udemy-5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace udemy_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // key - value, key gk boleh sama
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+
+            employees.Add(101, "Jane Doe");
+            employees.Add(102, "Bob Smith");
+
+            Console.WriteLine("Employees:\n");
+            foreach (KeyValuePair<int, string>  item in employees)
+            {
+                Console.WriteLine($"Employee ID: {item.Key}, Name: {item.Value}");
+            }
+        }
+    }
+}
