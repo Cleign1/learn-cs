3c6517d30d1c714fc1c82878beec302bcc285936
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-07-01 09:39:08 +0700

    course 151

---
+++ udemy-4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace udemy_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> nomor = new List<String>();
+
+            nomor.Add("1");
+            nomor.Add("2");
+            nomor.Add("3");
+            Console.WriteLine("Nomor di dalam list ada:");
+            foreach (var item in nomor)
+            {
+                Console.WriteLine(item);
+            }
+            nomor.Remove("1");
+            Console.WriteLine("Nomor di dalam list ada:");
+            foreach (var item in nomor)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
