c89e68fb28d2e8d05e7545494c7f198b736b27b1
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-07-01 16:50:19 +0700

    coding exercise section 7, coding exercise 16,

---
+++ StudentCollectionExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentCollectionExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Student> students = new Dictionary<string, Student>()
+            {
+                {"John", new Student(1, "John", 85)},
+                {"Alice", new Student(2, "Alice", 90)},
+                {"Bob", new Student(3,"Bob", 78)}
+            };
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"Name: {item.Key}, Id: {item.Value.ID}, Grade: {item.Value.Grade}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Grade { get; set; }
+        public Student(int id, string name, int grade)
+        {
+            ID = id;
+            Name = name;
+            Grade = grade;
+        }
+    }
+}
