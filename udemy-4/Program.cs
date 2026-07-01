4f5c20b87e697c3c815339a0a037db1a634a4181
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-07-01 10:21:11 +0700

    course 153

--- udemy-4/Program.cs
+++ udemy-4/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            List<int> nomor = new List<int>() { 10, 15, 5, 19, 3, 11,1};
+            List<int> nomor = new List<int>() { 10, 15, 5, 19, 3, 11,1, 99, 25, 9, 2, 3, 6};
 
             Console.WriteLine("Unsorted List");
             foreach (int item in nomor)
@@ -12,6 +12,15 @@
                 Console.Write($"{item}, ");
             }
 
+            Console.WriteLine(" ");
+            Console.WriteLine("semua nomor lebih tinggi dari 25");
+            List<int> nomorLebihDariSepuluh = nomor.FindAll(x => x >= 25);
+            foreach (int item in nomorLebihDariSepuluh)
+            {
+                Console.Write($"{item}, ");
+            }
+
+            Console.WriteLine(" ");
             nomor.Sort();
             Console.WriteLine("Sorted List");
             foreach (int item in nomor)
