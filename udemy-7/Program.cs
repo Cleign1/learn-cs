89047a23cb7da2f514752deb7de07df4565a038e
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-07-08 08:45:13 +0700

    new inheritance couse 180

---
+++ udemy-7/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace udemy_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Dog myDog = new Dog();
+            Console.WriteLine("Dog. Eat!");
+            myDog.Eat();
+            myDog.Bark();
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eeating...");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking....");
+        }
+    }
+}
