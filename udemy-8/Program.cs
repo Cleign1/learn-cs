c4c878010c859da56f4e6c39d8c23ad5c5a433bf
Ibnu Khaidar <ibnukhaidar@gmail.com>
2026-07-13 22:33:37 +0700

    course 188

---
+++ udemy-8/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace udemy_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee alice = new Employee("Alice", 25);
+        alice.DisplayPersonInfo();
+    }
+}
+
+public class Person
+{
+    public int Age { get; private set; }
+    public string Name { get; private set; }
+    
+    public Person(int age, string name)
+    {
+        Age = age;
+        Name = name;
+        Console.WriteLine("Person Constructor Called");
+    }
+    
+    public void DisplayPersonInfo()
+    {
+        Console.WriteLine($"This Person Name is {Name} and Age {Age}");
+    }
+}
+
+public class Employee : Person
+{
+    public Employee(string name, int age) : base(age, name)
+    {
+        Console.WriteLine("Employee Constructor Called");
+    }
+}
